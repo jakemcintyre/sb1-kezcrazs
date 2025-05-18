583595792277ea2af1a3c71c2dc86c795dc98152
jakemcintyre <jake.kecp@gmail.com>
2025-05-18 07:26:46 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 export default function App() {
-  return (
-    import React from 'react';
+  return (React
+    import  from 'react';
 
 export default function App() {
   return (
